1dc7a61324df4617eb8c13674f223e493f30b711
FDM-Tech <patrick.gottberg@online.de>
2024-11-13 21:22:22 +0100

    Update VideoPage.tsx

--- fbw-a380x/src/systems/instruments/src/SD/Pages/Video/VideoPage.tsx
+++ fbw-a380x/src/systems/instruments/src/SD/Pages/Video/VideoPage.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { PageTitle } from '../Generic/PageTitle';
-import MapViewWasmModule from './MapViewWasmModule.wasm';
-
 import '../../../index.scss';
 
-export const VideoPage = () => {
+export const VideoPage = () => (
   //const MapViewAircraft = MapViewAircraft();
 
   <PageTitle x={6} y={29}>
     VIDEO
-  </PageTitle>;
-
-  {
-    MapViewWasmModule.wasm_gauge('MapView2D');
-  }
-};
+  </PageTitle>
+);
